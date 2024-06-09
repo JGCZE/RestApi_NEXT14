6810030b4dfc0043961912b6a82a5fa9b800a9f8
Jan Gross <jan.gross.cze@gmail.com>
2024-06-09 16:29:37 +0200

    POST method

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,31 @@
 import { NextResponse } from "next/server"
+import connect from "@/lib/db";
+import User from "@/lib/modals/user";
 
-export const GET = () => {
-  return new NextResponse("this is new response from first api")
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200})
+  } catch (error: any){
+    return new NextResponse("Error in fetching users " + error.message, { status: 500 })
+  }
+}
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json()
+    await connect()
+    const newUser = new User(body)
+    await newUser.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "user is created", user: newUser }),
+      { status: 200 })
+
+  } catch (error: any) {
+    return new NextResponse("Error in creating user " + error.message, {
+      status: 500,
+    })
+  }
 }
